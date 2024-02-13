67b6fc3bfbfc77bee7a853c5ad201d05300d4570
Davide <bacilieri.davide@gmail.com>
2024-02-13 10:20:02 +0100

    basic procedures turned into functions

--- project/basic.py
+++ project/basic.py
@@ -2,124 +2,110 @@ from qtealeaves.emulator.mps_simulator import MPS
 from qtealeaves.tensors import QteaTensor as QteaT
 from qtealeaves.convergence_parameters.conv_params import TNConvergenceParameters as TNConvPar
 import numpy as np
+from copy import deepcopy
 
-def print_mps(mps): #helper func, to check the values 
-    print([i.elem for i in mps.tensors])
-    print("\n")
+def mpd_from_mps(mps):
+    my_mps = deepcopy(mps)
+    N = my_mps.num_sites
+    my_mps._convergence_parameters.sim_params["max_bond_dimension"]=2
+    # we need the isometry at the first site
+    ## move it all the way to make sure the mps is all reduced to bond dimension 2
+    my_mps.iso_towards(len(my_mps)-1)
+    my_mps.iso_towards(0, trunc=True)
+    my_mps.normalize()
     
-def print_mpd(mpd):
-    print([i.elem for i in mpd])
-    print("\n")
-
-
-H = QteaT.from_elem_array(np.array([[1,1],[1,-1]])/(2**0.5))
-CNOT = QteaT.from_elem_array(np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]]).reshape((2,2,2,2)))
-NCNOT = QteaT.from_elem_array(np.array([[0,1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]]).reshape((2,2,2,2)))
-HY = QteaT.from_elem_array(np.array([[1,1j],[1,-1j]])/(2**0.5))
-
-N = 8 # number of sites
-d = 2 # local dimension
-chi = 2 # bond dimension
-control = "R" # switch between initializations
-
-if control=="R":
-    # random
-    my_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="random", local_dim=d)
-
-elif control=="G":
-    # GHZ
-    my_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
-    my_mps.apply_one_site_operator(H,0)
-    for n in range(N-1):
-        my_mps.apply_two_site_operator(CNOT,n)
-
-elif control=="A":
-    # alternative GHZ - "simple" non-symmetric state
-    my_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
-    my_mps.apply_one_site_operator(H,0)
-    for n in range(N-1):
-        my_mps.apply_two_site_operator(NCNOT,n)    
-
-
-my_mps.iso_towards(0) 
-#my_mps.iso_towards(N-1)
-my_mps.normalize()
-#print_mps(my_mps)
-
-my_mpd = []
-
-# start from G[0]
-G = QteaT((d,d,d,d)) # initialized to zero
-G.elem[0,0,:,:]=my_mps[0].copy().elem # set the first part equal to the MPS  
-for i in range(d):
-    for j in range(d):
-        if j==0 and i==0: # don't reset the first part
-            continue
-        M = QteaT((d,d), ctrl="R") #random init
-        
-        # take random matrix, keep only parts perpendicular to the ones that are already set, renormalize
-        ##the parts that aren't set yet will just give a null contribution to this process
-        for k in range(d):
-            for h in range(d):
-                M.elem -= G.elem[k,h,:,:]*np.vdot(G.elem[k,h,:,:],M.elem)
-                M.normalize()
-        G.elem[i,j,:,:] = M.elem
-
-my_mpd.append(G)
-
-# same for G[1-6]
-for n in range(1,N-1):
-    G = QteaT((d,d,d,d))
-    G.elem[0,:,:,:]=my_mps[n].copy().elem
+    my_mpd = []
     
-    for i in range(1,d):
+    # start from G[0]
+    G = QteaT((d,d,d,d)) # initialized to zero
+    G.elem[0,0,:,:]=my_mps[0].copy().elem # set the first part equal to the MPS  
+    for i in range(d):
         for j in range(d):
-            M = QteaT((d,d), ctrl="R")
+            if j==0 and i==0: # don't reset the first part
+                continue
+            M = QteaT((d,d), ctrl="R") #random init
+            
+            # take random matrix, keep only parts perpendicular to the ones that are already set, renormalize
+            ##the parts that aren't set yet will just give a null contribution to this process
             for k in range(d):
                 for h in range(d):
-                    M.elem -= G.elem[k,h,:,:]*np.vdot(G.elem[k,h,:,:], M.elem)
+                    M.elem -= G.elem[k,h,:,:]*np.vdot(G.elem[k,h,:,:],M.elem)
                     M.normalize()
             G.elem[i,j,:,:] = M.elem
     
     my_mpd.append(G)
+    
+    # same for G[1:N-2]
+    for n in range(1,N-1):
+        G = QteaT((d,d,d,d))
+        G.elem[0,:,:,:]=my_mps[n].copy().elem
+        
+        for i in range(1,d):
+            for j in range(d):
+                M = QteaT((d,d), ctrl="R")
+                for k in range(d):
+                    for h in range(d):
+                        M.elem -= G.elem[k,h,:,:]*np.vdot(G.elem[k,h,:,:], M.elem)
+                        M.normalize()
+                G.elem[i,j,:,:] = M.elem
+        
+        my_mpd.append(G)
+    
+    # now for G[N-1] 
+    ##remove extra leg with dimension 1 to avoid errors in apply_one_site_operator
+    my_mpd.append(my_mps[N-1].copy().remove_dummy_link(2))
+    
+    return my_mpd
 
-# now for G[7] 
-##remove extra leg with dimension 1 to avoid errors in apply_one_site_operator
-my_mpd.append(my_mps[N-1].copy().remove_dummy_link(2))
+def rev_apply_mpd(mpd, mps=None, chi=2, d=2):
+    if mps is None:
+        N = len(mpd[0])
+        mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
+    else:
+        mps = deepcopy(mps)
+        N = len(mps)
+    
+    mps.normalize()
+    for i in range(len(mpd)):
+        mps.apply_one_site_operator(mpd[i][N-1],N-1)
+        for n in reversed(range(N-1)):
+            mps.apply_two_site_operator(mpd[i][n].transpose((1,0,2,3)),n)
 
+    mps.normalize()
+    mps.iso_towards(0)
+    return mps
+    
+def apply_mpd(mpd, mps=None, chi=2, d=2):
+    if mps is None:
+        N = len(mpd[0])
+        mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
+    else:
+        mps = deepcopy(mps)
+        N = len(mps)
+    
+    mps.normalize()
+    for i in reversed(range(len(mpd))):
+        for n in range(N-1):
+            mps.apply_two_site_operator(my_mpd[i][n].transpose((2,3,1,0)).conj(),n) 
+    
+        mps.apply_one_site_operator(my_mpd[i][N-1].transpose((1,0)).conj(),N-1)
+    mps.normalize()
+    mps.iso_towards(0)
+    return mps
 
-## now check results
-
-control_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
-control_mps.normalize()
-# initialized to zero on all sites
-
-# apply the MPD a gate at a time, see notes for how the circuit is built
-"""
-my_mps.apply_one_site_operator(my_mpd[N-1],N-1)
-for n in reversed(range(N-1)):
-    my_mps.apply_two_site_operator(my_mpd[n],n,swap=True)
-
-my_mps.normalize()
-print_mps(my_mps)
-"""
-
-for n in range(N-1):
-    control_mps.apply_two_site_operator(my_mpd[n].transpose((2,3,1,0)).conj(),n)
-
-control_mps.apply_one_site_operator(my_mpd[N-1].transpose((1,0)).conj(),N-1)
-
-control_mps.normalize()
-
-control_mps.iso_towards(0)
-#print_mps(control_mps)
-
-
-#print_mpd(my_mpd)
-
-
-# print fidelity
-print(np.abs(control_mps.contract(my_mps))) 
-
-# GHZ : quite low, ~ 0.3 - 0.5
-# random : very low, ~ 0.02
+    
+if __name__ == "__main__":
+    d=2
+    chi=3
+    N=8
+    my_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="random", local_dim=d)
+    my_mpd = []
+    my_mpd.append(mpd_from_mps(my_mps))
+    
+    
+    control_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
+    control_mps.normalize()
+    
+    print(np.abs(my_mps.contract(apply_mpd(my_mpd, control_mps))))
+    print(np.abs(control_mps.contract(rev_apply_mpd(my_mpd, my_mps))))
+    
