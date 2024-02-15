915019b58712440c565a704fa71a2a0b4ffc70d3
Davide <bacilieri.davide@gmail.com>
2024-02-15 16:21:56 +0100

    update optimization logic

--- project/higher.py
+++ project/higher.py
@@ -2,7 +2,6 @@ from qtealeaves.emulator import MPS
 from qtealeaves.convergence_parameters import TNConvergenceParameters as TNConvPar
 from basic import *
 from qtealeaves.tensors import QteaTensor as QteaT
-from cmath import phase, exp as cexp
 
 def full_analytic_mpd(mps, D):
     my_mpd = []
@@ -56,31 +55,76 @@ def optimize_mpd(mpd, mps, layers=None, l_rate=0.005, tol=0.05, maxiter=1000):
                 mps_b_n = rev_apply_mpd([mpd_b], mps_b)
                 
                 if n==N-1: # two-leg tensor case
-                    f = mps_a_n.contract(mps_b_n, (0,N-1)).remove_dummy_link(0)
-                    dtens = mps_a_n[N-1].remove_dummy_link(2).tensordot(f, (0,0)).tensordot(mps_b_n[N-1].conj().remove_dummy_link(2), (1,0))
+                    c = mps_a_n.contract(mps_b_n, (0,N-1)).remove_dummy_link(0)
+                    f = mps_a_n[N-1].remove_dummy_link(2).tensordot(c, (0,0)).tensordot(mps_b_n[N-1].conj().remove_dummy_link(2), (1,0))
+                    f = f.transpose((1,0))
                     
                 elif n==N-2: # right edge case
-                    f = mps_a_n.contract(mps_b_n, (0,N-2)).remove_dummy_link(0) 
+                    c = mps_a_n.contract(mps_b_n, (0,N-2)).remove_dummy_link(0) 
                     twoq_a = mps_a_n[N-2].tensordot(mps_a_n[N-1].remove_dummy_link(2), (2,0))
                     twoq_b = mps_b_n[N-2].tensordot(mps_b_n[N-1].remove_dummy_link(2), (2,0))
-                    dtens = twoq_a.tensordot(f.tensordot(twoq_b.conj(), (1,0)), (0,0))    
+                    f = twoq_a.tensordot(c.tensordot(twoq_b.conj(), (1,0)), (0,0))
+                    f = f.transpose((2,3,0,1))
                     
                 elif n==0: # left edge case
-                    f = mps_a_n.contract(mps_b_n, (N-1,1)).remove_dummy_link(2) 
+                    c = mps_a_n.contract(mps_b_n, (N-1,1)).remove_dummy_link(2) 
                     twoq_a = mps_a_n[0].remove_dummy_link(0).tensordot(mps_a_n[1], (1,0))
                     twoq_b = mps_b_n[0].remove_dummy_link(0).tensordot(mps_b_n[1], (1,0))
-                    dtens = twoq_a.tensordot(f, (2,0)).tensordot(twoq_b.conj(), (2,2))
+                    f = twoq_a.tensordot(c, (2,0)).tensordot(twoq_b.conj(), (2,2))
+                    f = f.transpose((2,3,0,1))
                 
                 else: # in between
-                    f1 = mps_a_n.contract(mps_b_n, (0,n)).remove_dummy_link(0) 
-                    f2 = mps_a_n.contract(mps_b_n, (N-1,n+1)).remove_dummy_link(2)
+                    c1 = mps_a_n.contract(mps_b_n, (0,n)).remove_dummy_link(0) 
+                    c2 = mps_a_n.contract(mps_b_n, (N-1,n+1)).remove_dummy_link(2)
                     twoq_a = mps_a_n[n].tensordot(mps_a_n[n+1], (2,0))
                     twoq_b = mps_b_n[n].tensordot(mps_b_n[n+1], (2,0))
-                    dtens = twoq_a.tensordot(f2, (3,0)).tensordot(f1.tensordot(twoq_b.conj(), (1,0)), ([0,3],[0,3]))
+                    f = twoq_a.tensordot(c2, (3,0)).tensordot(c1.tensordot(twoq_b.conj(), (1,0)), ([0,3],[0,3]))
+                    f = f.transpose((2,3,0,1))
                 
+                """ # gradient ascent kinda thing, not working
+                new_mpd[l][n].elem += f.elem*l_rate*contr.conjugate()
                 
-                new_mpd[l][n].elem += dtens.elem*l_rate*cexp(-1j*phase(contr))
-                new_mpd[l][n].normalize()
+                #new_mpd[l][n].normalize() #nope, not normalized, it needs to be _unitary_
+                if n==N-1:
+                    t_left, t_right, _, _ = new_mpd[l][n].split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
+                    new_mpd[l][n] = t_left.tensordot(t_right, (1,0))
+                else:
+                    t_left, t_right, _, _ = new_mpd[l][n].split_svd([0,1],[2,3], conv_params=TNConvPar(), no_truncation=True)
+                    new_mpd[l][n] = t_left.tensordot(t_right, (2,0))
+                """
+                if n!=N-1:
+                    # reshape tensor in matrix form in the 4-leg cases
+                    f = f.reshape((d**2, d**2))
+                
+                # make it unitary
+                t_left, t_right, _, _ = f.split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
+                unew = t_left @ t_right
+                
+                # get "old" tensor, reshape if needed
+                if n!=N-1:
+                    u = new_mpd[l][n].reshape((d**2,d**2))
+                else:
+                    u = new_mpd[l][n]
+                
+                # find adj of the old tensor
+                uadj = u.transpose((1,0)).conj()
+                
+                # calculate (uadj@unew)**lrate
+                tens = uadj @ unew
+                t_left, t_right, singvals, _ = tens.split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
+                singvals = singvals ** l_rate
+                tens = t_left @ QteaT.from_elem_array(np.diag(singvals)) @ t_right
+                
+                # u' = u (uadj@unew)**lrate
+                uprime = u @ tens
+                
+                # reshape if needed, assign
+                if n!=N-1:
+                    uprime = uprime.reshape((d,d,d,d))
+                
+                new_mpd[l][n] = uprime
+                
+                    
         mpd=new_mpd
     
     return mpd, fid
@@ -103,7 +147,7 @@ if __name__=="__main__":
     print(-np.log(np.abs(my_mps.contract(apply_mpd(my_mpd, control_mps))))/N)
     print(-np.log(np.abs(control_mps.contract(rev_apply_mpd(my_mpd, my_mps))))/N)
     
-    my_mpd, fid = optimize_mpd(my_mpd, my_mps, layers=(0,1), maxiter=400)
+    my_mpd, fid = optimize_mpd(my_mpd, my_mps, layers=(0,1), maxiter=100, l_rate=0.06)
     
     print()
     print(-np.log(np.abs(my_mps.contract(apply_mpd(my_mpd, control_mps))))/N)
