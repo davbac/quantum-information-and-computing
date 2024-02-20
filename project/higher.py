67d626c7522e56a1340f549ffa0d47f40f57bd6c
Davide <bacilieri.davide@gmail.com>
2024-02-20 17:21:26 +0100

    finished rewriting the code, same results as original one

--- project/higher.py
+++ project/higher.py
@@ -12,135 +12,102 @@ def full_analytic_mpd(mps, D):
     return my_mpd
 
 def optimize_mpd(mpd, mps, layers=None, l_rate=0.001, tol=0.05, maxiter=1000): 
-    mpd = deepcopy(mpd)
-    if layers is None:
-        # by default only optimize the last layer of the MPD 
-        layers = (len(mpd)-1,)
-        
-    N = mps.num_sites
-    chi = mps._convergence_parameters.sim_params["max_bond_dimension"]
-    if np.all(np.array(mps.local_dim) == mps.local_dim[0]):
-        d = mps.local_dim[0]
-    else:
-        raise Exception("Needs an MPS with the same local dimension on all sites. Tested only on d=2")
+    # a few parameters
+    N=mps.num_sites
+    chi=mps._convergence_parameters.sim_params["max_bond_dimension"]
+    d=np.random.choice(mps.local_dim)
+    if not np.allclose(mps.local_dim,d):
+        raise Exception("This algorithm only works with the same physical dimension across loci")
     
-    control_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=d)
-    control_mps.normalize()
-    
-    II = QteaT.from_elem_array(np.eye(d**2).reshape((d,d,d,d)))
+    # define some useful quantities
+    control_mps = MPS(N, TNConvPar(max_bond_dimension=chi), initialize="vacuum", local_dim=int(d))
     I = QteaT.from_elem_array(np.eye(d))
+    II = QteaT.from_elem_array(np.eye(d**2).reshape((d,d,d,d)))
     
     fid = []
-    for count in range(maxiter):
-        contr = mps.contract(apply_mpd(mpd, control_mps))
+    
+    for it in range(maxiter): #start iterating
+        #check convergence
+        contr = control_mps.contract(apply_mpd(mpd, mps, rev=True))
         fid.append(np.abs(contr))
-        if fid[-1]>1-tol:
+        if np.abs(contr)>1-tol:
             break
         
+        if it%10 == 9:
+            print(it, "\t", fid[-1])
+        
         new_mpd = deepcopy(mpd)
+        
         for l in layers:
-            mps_a = apply_mpd(mpd[:l],control_mps) #, adj=True) #try adj
-            mps_b = apply_mpd(mpd[l+1:], mps, rev=True)
+            ## Get the result of the circuit "up to the given layer" on both sides
+            
+            mpd_f = mpd[l+1:] # "forward" part, applied to control_mps
+            mpd_r = mpd[:l]   # "reverse" part, applied to the given mps
+            
+            mps_f = apply_mpd(mpd_f, control_mps)
+            mps_r = apply_mpd(mpd_r, mps, rev=True)
+            
             for n in range(N):
-                mpd_a = [II for _ in range(n+1)]
-                mpd_a.extend(mpd[l][n+1:])
-                if n == N-1:
-                    mpd_a[-1]=I 
-                
-                mpd_b = mpd[l][:n]
-                mpd_b.extend([II for _ in range(N-n-1)])
-                mpd_b.append(I) 
-                
-                mps_a_n = apply_mpd([mpd_a], mps_a)#, adj=True) #try adj
-                mps_b_n = apply_mpd([mpd_b], mps_b, rev=True)
-                
-                if n==N-1: # two-leg tensor case
-                    c = mps_a_n.contract(mps_b_n, (0,N-1)).remove_dummy_link(0)
-                    f = mps_a_n[N-1].remove_dummy_link(2).tensordot(c, (0,0)).tensordot(mps_b_n[N-1].conj().remove_dummy_link(2), (1,0))
-                    #f = f.transpose((1,0))
-                    
-                elif n==N-2: # right edge case
-                    c = mps_a_n.contract(mps_b_n, (0,N-2)).remove_dummy_link(0) 
-                    twoq_a = mps_a_n[N-2].tensordot(mps_a_n[N-1].remove_dummy_link(2), (2,0))
-                    twoq_b = mps_b_n[N-2].tensordot(mps_b_n[N-1].remove_dummy_link(2), (2,0))
-                    f = twoq_a.tensordot(c.tensordot(twoq_b.conj(), (1,0)), (0,0))
-                    #f = f.transpose((2,3,0,1))
-                    
-                elif n==0: # left edge case
-                    c = mps_a_n.contract(mps_b_n, (N-1,1)).remove_dummy_link(2) 
-                    twoq_a = mps_a_n[0].remove_dummy_link(0).tensordot(mps_a_n[1], (1,0))
-                    twoq_b = mps_b_n[0].remove_dummy_link(0).tensordot(mps_b_n[1], (1,0))
-                    f = twoq_a.tensordot(c, (2,0)).tensordot(twoq_b.conj(), (2,2))
-                    #f = f.transpose((2,3,0,1))
-                
-                else: # in between
-                    c1 = mps_a_n.contract(mps_b_n, (0,n)).remove_dummy_link(0) 
-                    c2 = mps_a_n.contract(mps_b_n, (N-1,n+1)).remove_dummy_link(2)
-                    twoq_a = mps_a_n[n].tensordot(mps_a_n[n+1], (2,0))
-                    twoq_b = mps_b_n[n].tensordot(mps_b_n[n+1], (2,0))
-                    f = twoq_a.tensordot(c2, (3,0)).tensordot(c1.tensordot(twoq_b.conj(), (1,0)), ([0,3],[0,3]))
-                    #f = f.transpose((2,3,0,1))
-                
-                # gradient ascent kinda thing, not working
-                new_mpd[l][n].elem += f.elem.conj()*l_rate*contr.conjugate()
-                
-                #new_mpd[l][n].normalize() #nope, not normalized, it needs to be _unitary_
+                mpd_f_n = [[II for _ in range(n+1)] + mpd[l][n+1:]]
                 if n==N-1:
-                    a=new_mpd[l][n].elem.copy()
-                else:
-                    a=new_mpd[l][n].elem.reshape((d**2,d**2)).copy()
+                    mpd_f_n[0][-1]=I 
                 
-                for i in range(len(a)):
-                    a[i]/=np.vdot(a[i],a[i])**0.5
+                mpd_r_n = [mpd[l][:n] + [II for _ in range(N-n-1)] + [I]]
                 
-                if n==N-1:
-                    new_mpd[l][n].elem=a
+                mps_f_n = apply_mpd(mpd_f_n, mps_f)
+                mps_r_n = apply_mpd(mpd_r_n, mps_r, rev=True)
+                
+                mps_f_n.iso_towards(n) # not needed
+                mps_r_n.iso_towards(n) # (probably)
+                
+                if n>0:
+                    c1 = mps_r_n.contract(mps_f_n, (0,n)).remove_dummy_link(0)
+                else: 
+                    c1 = mps_r_n[0].eye_like(mps_r_n[0].links[0])
+                if n<N-2:
+                    c2 = mps_r_n.contract(mps_f_n, (N-1, n+1)).remove_dummy_link(2)
                 else:
-                    new_mpd[l][n].elem=a.reshape((d,d,d,d))
-                """
-                if n == N-1:
-                    t_left, t_right, _, _ = new_mpd[l][n].split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
-                    new_mpd[l][n] = t_left.tensordot(t_right, 1)
-                else:
-                    t_left, t_right, _, _ = new_mpd[l][n].split_svd([0,1],[2,3], conv_params=TNConvPar(), no_truncation=True)
-                    new_mpd[l][n] = t_left.tensordot(t_right, 1)
-                """
-                """
+                    c2 = mps_r_n[N-1].eye_like(mps_r_n[N-1].links[2])
+                    
+                
                 if n!=N-1:
-                    # reshape tensor in matrix form in the 4-leg cases
+                    q_f = QteaT.__matmul__(*mps_f_n[n:n+2])
+                    q_r = QteaT.__matmul__(*mps_r_n[n:n+2])
+                else:
+                    q_f = mps_f_n[n]
+                    q_r = mps_r_n[n]
+                
+                f = c1.tensordot(q_r, (0,0)).tensordot(c2, (-1,0)).tensordot(q_f.conj(), ([0,-1],[0,-1]))
+                #f = c1.transpose((1,0)).tensordot(q_f.conj(), (0,0)).tensordot(c2.transpose((1,0)), (-1,0)).tensordot(q_r,([0,-1],[0,-1]))
+                
+                
+                if n!=N-1:
+                    #f = f.transpose((1,0,3,2))
+                    print(f.tensordot(mpd[l][n], ([0,1,2,3],[0,1,2,3])).elem, contr) #should be equal??
                     f = f.reshape((d**2, d**2))
-                
-                # make it unitary
-                t_left, t_right, _, _ = f.split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
-                unew = t_left @ t_right
-                
-                # get "old" tensor, reshape if needed
-                if n!=N-1:
-                    u = new_mpd[l][n].reshape((d**2,d**2))
+                    u = mpd[l][n].reshape((d**2, d**2))
                 else:
-                    u = new_mpd[l][n]
+                    u = mpd[l][n]
                 
-                # find adj of the old tensor
-                uadj = u.transpose((1,0)).conj()
+                #f = f.transpose((1,0))
+                #f = f.conj()
                 
-                # calculate (uadj@unew)**lrate
-                tens = uadj @ unew
-                t_left, t_right, singvals, _ = tens.split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
-                singvals = singvals ** l_rate
-                tens = t_left @ QteaT.from_elem_array(np.diag(singvals)) @ t_right
+                f_l, f_r, _, _ = f.split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
+                f = f_l @ f_r 
                 
-                # u' = u (uadj@unew)**lrate
-                uprime = u @ tens
+                m = u.transpose((1,0)).conj() @ f
+                m_l, m_r, vals, _ = m.split_svd([0],[1], conv_params=TNConvPar(), no_truncation=True)
+                vals = QteaT.from_elem_array(np.diag(vals**l_rate))
+                m = m_l @ vals @ m_r 
+                uprime = u @ m
                 
-                # reshape if needed, assign
                 if n!=N-1:
                     uprime = uprime.reshape((d,d,d,d))
                 
                 new_mpd[l][n] = uprime
-                """
-                    
-        mpd=new_mpd
-    
+        
+        mpd = new_mpd
+                
     return mpd, fid
 
 if __name__=="__main__":
@@ -161,7 +128,7 @@ if __name__=="__main__":
     print(-np.log(np.abs(my_mps.contract(apply_mpd(my_mpd, control_mps))))/N)
     print(-np.log(np.abs(control_mps.contract(apply_mpd(my_mpd, my_mps, rev=True))))/N)
     
-    my_mpd, fid = optimize_mpd(my_mpd, my_mps, layers=(0,1))#, maxiter=100, l_rate=0.06)
+    my_mpd, fid = optimize_mpd(my_mpd, my_mps, layers=(0,1), maxiter=50, l_rate=0.6)
     
     print()
     print(-np.log(np.abs(my_mps.contract(apply_mpd(my_mpd, control_mps))))/N)
